30ea4f9ec3cca21126833c5052029fcc4fab68d8
Thomas <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2017-11-17 09:46:03 +0100

    reformat

--- src/main/java/tvestergaard/databaseassignment/database/users/UnknownUserIdException.java
+++ src/main/java/tvestergaard/databaseassignment/database/users/UnknownUserIdException.java
@@ -3,28 +3,28 @@ package tvestergaard.databaseassignment.database.users;
 public class UnknownUserIdException extends UserDAOException
 {
 
-	/**
-	 * The unknown unknownId.
-	 */
-	private int unknownId;
+    /**
+     * The unknown unknownId.
+     */
+    private int unknownId;
 
-	/**
-	 * Creates a new {@link UnknownUserIdException}.
-	 *
-	 * @param unknownId The unknown unknownId.
-	 */
-	public UnknownUserIdException(int unknownId)
-	{
-		this.unknownId = unknownId;
-	}
+    /**
+     * Creates a new {@link UnknownUserIdException}.
+     *
+     * @param unknownId The unknown unknownId.
+     */
+    public UnknownUserIdException(int unknownId)
+    {
+        this.unknownId = unknownId;
+    }
 
-	/**
-	 * Returns the unknown unknownId.
-	 *
-	 * @return The unknown unknownId.
-	 */
-	public int getUnknownId()
-	{
-		return this.unknownId;
-	}
+    /**
+     * Returns the unknown unknownId.
+     *
+     * @return The unknown unknownId.
+     */
+    public int getUnknownId()
+    {
+        return this.unknownId;
+    }
 }
--- src/test/java/tvestergaard/databaseassignment/database/users/UnknownUsernameExceptionTest.java
+++ src/test/java/tvestergaard/databaseassignment/database/users/UnknownUsernameExceptionTest.java
@@ -6,11 +6,11 @@ import static org.junit.Assert.assertSame;
 
 public class UnknownUsernameExceptionTest
 {
-	@Test
-	public void getUnknownUsername() throws Exception
-	{
-		String                   expected  = "Username";
-		UnknownUsernameException exception = new UnknownUsernameException(expected);
-		assertSame(expected, exception.getUnknownUsername());
-	}
+    @Test
+    public void getUnknownUsername() throws Exception
+    {
+        String expected = "Username";
+        UnknownUsernameException exception = new UnknownUsernameException(expected);
+        assertSame(expected, exception.getUnknownUsername());
+    }
 }
